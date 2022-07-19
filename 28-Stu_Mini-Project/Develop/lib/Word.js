1bba7a11ab6ab15d540b4e211ebab4b4dc7c3762
Sean Oh <seanoh@ucsb.edu>
2022-07-19 13:50:05 -0700

    add Word constructor and guessLetter method

--- 28-Stu_Mini-Project/Develop/lib/Word.js
+++ 28-Stu_Mini-Project/Develop/lib/Word.js
@@ -1,7 +1,22 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+  constructor(word) {
+    this.letters = [];
+    for(var i = 0; i < word.length; i++) {
+      this.letters.push(new Letter(word[i]));
+    }
+  }
+
+  guessLetter(letter) {
+    for(var i of this.letters) {
+      if(i.guess(letter)){
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
 
 module.exports = Word;
