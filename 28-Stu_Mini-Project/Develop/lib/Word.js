525f41fb008aa3081bbea8b4c539202ecfd8cd45
Sean Oh <seanoh@ucsb.edu>
2022-07-19 14:28:39 -0700

    add guessedCorrectly method

--- 28-Stu_Mini-Project/Develop/lib/Word.js
+++ 28-Stu_Mini-Project/Develop/lib/Word.js
@@ -6,15 +6,32 @@ class Word {
     for(var i = 0; i < word.length; i++) {
       this.letters.push(new Letter(word[i]));
     }
+    this.guesses = [];
   }
 
   guessLetter(letter) {
+    var isCorrect = false;
+
     for(var i of this.letters) {
       if(i.guess(letter)){
-        return true;
+        isCorrect = true;
       }
     }
-    return false;
+
+    if(!this.guesses.includes(letter)){
+      this.guesses.push(letter);
+    }
+    
+    return isCorrect;
+  }
+
+  guessedCorrectly() {
+    for(var i = 0; i < this.guesses.length; i++) {
+      if(!this.guessLetter(this.guesses[i])){
+        return false;
+      }
+    }
+    return true;
   }
 
 }
