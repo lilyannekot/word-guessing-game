e8d6cc01cb8ca9dc6bf27ae00113f26223a10b62
Sean Oh <seanoh@ucsb.edu>
2022-07-19 13:14:40 -0700

    add Letter constructor

--- 28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,16 @@
 class Letter {
-  
+  constructor(character) {
+    this.character = character;
+
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+
+    if( !(letters.split("").includes(this.character) || numbers.split("").includes(this.character)) ) {
+      this.visible = true;
+    } else {
+      this.visible = false;
+    }
+  }
 }
 
 module.exports = Letter;
